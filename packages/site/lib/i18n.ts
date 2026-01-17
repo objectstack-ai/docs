101b6729f03845a20d3b8e5b810997553bbe8074
Jack <zhuangjianguo@gmail.com>
2026-01-17 19:12:00 +0800

    Move docs to language-specific directories
    Renamed documentation files from generic framework/objectos/objectql/objectui paths to language-specific 'en' directories to support internationalization. Also removed the 'index' page from the docs meta.json and cleaned up unused i18n config options.

--- packages/site/lib/i18n.ts
+++ packages/site/lib/i18n.ts
@@ -5,6 +5,4 @@ export const i18n = defineI18n({
   defaultLanguage: siteConfig.i18n.defaultLanguage,
   languages: siteConfig.i18n.languages,
   parser: 'dir',  
-  hideLocale: 'default-locale',
-
 });
